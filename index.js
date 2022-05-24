4534bbf8a6b1252763a364d8fb4660e3d82d4ffc
Evgeny Fedorenko <evgeny.fedorenko@constant.obninsk.ru>
2022-05-24 16:10:38 +0300

    fix : fix 07

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ async function postPost() {
     headers: {
       'Content-Type': 'application/json',
       'Authorization': `${APP_KEY}`,
-      'referer' : 'https://uapi.gravitec.net/api/v3/push'
+      'referer' : 'https://ticket-app-message.vercel.app/'
     },
     body: {
       "payload": {
