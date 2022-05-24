6d6dee4622c5bea69dc0969c1d0c71e93c7ff354
Evgeny Fedorenko <evgeny.fedorenko@constant.obninsk.ru>
2022-05-24 15:28:40 +0300

    fix : mode: 'no-cors'

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const btnAdd = document.querySelector('#onSendMessage')
 
 
 const url = 'https://uapi.gravitec.net/api/v3/push';
-const APP_KEY= '31ccf9c18f39626412bc4cea8fd91f89';
+const APP_KEY = '31ccf9c18f39626412bc4cea8fd91f89';
 const APP_SECRET = '21d2692a71af5368eaa9adbff1cbdf6f';
 // curl -X \
 //   -u "31ccf9c18f39626412bc4cea8fd91f89:21d2692a71af5368eaa9adbff1cbdf6f" \
@@ -17,32 +17,37 @@ const APP_SECRET = '21d2692a71af5368eaa9adbff1cbdf6f';
 //   https://uapi.gravitec.net/api/v3/push
 
 async function postPost() {
-  const res = await fetch('https://uapi.gravitec.net/api/v3/push', {
-    method: "POST",
+
+  const myHeaders = new Headers();
+  const myInit = {
+    method: 'POST',
     headers: {
       'Content-Type': 'application/json',
       'Authorization': `${APP_KEY}`,
       'Cache-Control': 'no-cache'
     },
-    body: JSON.stringify(
-      {
-        "payload": {
-          "message": " Test massage",
-          "title": "Test message",
-          "redirect_url": "https://gravitec.net",
+    mode: 'no-cors',
+    cache: 'default'
+  };
 
-        }
-      }
+  const myRequest = new Request({
+    "payload": {
+      "message": " Test massage",
+      "title": "Test message",
+      "redirect_url": "https://gravitec.net",
+    }
+  }, myInit);
 
-    )
-  }).then(res => res.json())
-  return res;
+ const result= await fetch(myRequest).then(function (response) {
+    return response.json()
+  })
+   return result
+
 }
+
 
 btnAdd.addEventListener('click', () => {
   postPost()
     .then(res => console.log(res))
     .catch(err => console.log(err))
 })
-
-
